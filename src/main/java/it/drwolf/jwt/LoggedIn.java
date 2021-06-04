6ac269cd707b9ba6c883840499c73b8b64ca4dd8
Andrea Agili <agea@drwolf.it>
2021-06-04 12:03:10 +0200

    :sparkles: :lock: LoggedIn Authenticator

---
+++ src/main/java/it/drwolf/jwt/LoggedIn.java
@@ -0,0 +1,18 @@
+package it.drwolf.jwt;
+
+import play.mvc.Http;
+import play.mvc.Security.Authenticator;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+public class LoggedIn extends Authenticator {
+
+    @Inject
+    private JWTUtils jwtUtils;
+
+    @Override
+    public Optional<String> getUsername(Http.Request req) {
+       return Optional.of(jwtUtils.getUser(req).toString());
+    }
+}
