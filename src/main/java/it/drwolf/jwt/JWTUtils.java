38eec8e1bbc1fce4cfe7520b7a50304b22f59ad5
Andrea Agili <agea@drwolf.it>
2021-06-04 16:24:33 +0200

    :bug: fic config injection

--- src/main/java/it/drwolf/jwt/JWTUtils.java
+++ src/main/java/it/drwolf/jwt/JWTUtils.java
@@ -16,7 +16,6 @@ import play.mvc.Http;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.lang.reflect.ParameterizedType;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 import java.util.List;
@@ -27,108 +26,104 @@ import java.util.concurrent.TimeUnit;
 @Singleton
 public class JWTUtils {
 
+	private final com.auth0.jwt.JWTVerifier delegate;
+	private final JwkProvider jwkProvider;
+	private Config config;
 
-    private final com.auth0.jwt.JWTVerifier delegate;
-    private final JwkProvider jwkProvider;
+	@Inject
+	public JWTUtils(Config config) {
+		this.config = config;
+		this.delegate = JWT.require(getAlgorithm()).withIssuer(getIssuer()).build();
+		this.jwkProvider = new JwkProviderBuilder(config.getString("jwks.domain")).build();
+	}
 
-    public JWTUtils() {
-        this.delegate = JWT.require(getAlgorithm()).withIssuer(getIssuer()).build();
-        this.jwkProvider = new JwkProviderBuilder(config.getString("jwks.domain")).build();
-    }
+	public String create(Object user) {
+		com.auth0.jwt.JWTCreator.Builder builder = JWT.create().withIssuer(getIssuer());
+		builder.withExpiresAt(getExpiration());
+		builder.withClaim(getUserClaim(), Json.toJson(user).toString());
+		return builder.sign(getAlgorithm());
 
-    public DecodedJWT verifyJWKS(String accessToken){
-        try {
-            DecodedJWT decodedJwt = JWT.decode(accessToken);
-            Jwk jwk = jwkProvider.get(decodedJwt.getKeyId());
-            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
-            Verification verifier = JWT.require(algorithm);
-            verifier.build().verify(decodedJwt);
-            return decodedJwt;
-        } catch (JwkException e) {
-            throw new HttpException("Error verifying JWKS Token", e);
-        }
-    }
+	}
 
-    @Inject
-    private Config config;
+	protected Algorithm getAlgorithm() {
+		return Algorithm.HMAC256(getSecret());
+	}
 
-    public String create(Object user) {
-        com.auth0.jwt.JWTCreator.Builder builder = JWT.create().withIssuer(getIssuer());
-        builder.withExpiresAt(getExpiration());
-        builder.withClaim(getUserClaim(), Json.toJson(user).toString());
-        return builder.sign(getAlgorithm());
+	protected Date getExpiration() {
+		return new Date(
+				new Date().getTime() + Optional.ofNullable(config.getDuration("jwt.expiration", TimeUnit.MILLISECONDS))
+						.orElse(8 * 3600000l));
+	}
 
-    }
+	protected String getIssuer() {
+		return Optional.ofNullable(config.getString("jwt.issuer")).orElse("drwolf.it");
+	}
 
-    protected Algorithm getAlgorithm() {
-        return Algorithm.HMAC256(getSecret());
-    }
+	protected String getSecret() {
+		return this.config.getString("play.http.secret.key");
+	}
 
-    protected String getSecret() {
-        return this.config.getString("play.http.secret.key");
-    }
+	private Optional<String> getTokenFromHeaders(Map<String, List<String>> headers) {
+		List<String> headerAuth = headers.get("Authorization");
+		if ((headerAuth != null) && (headerAuth.size() == 1) && (headerAuth.get(0) != null)) {
+			String authorization = headerAuth.get(0);
+			return Optional.of(authorization.replace("Bearer ", ""));
+		}
+		return Optional.empty();
+	}
 
-    protected String getUserClaim() {
-        return Optional.of(this.config.getString("jwt.userClaim")).orElse("user");
-    }
+	private Optional<String> getTokenFromQueryString(Map<String, String[]> queryString) {
+		if (queryString != null && queryString.containsKey("token")) {
+			String[] tokens = queryString.get("token");
+			if (tokens != null && tokens.length > 0) {
+				return Optional.of(tokens[0]);
+			}
+		}
+		return Optional.empty();
+	}
 
-    protected String getIssuer(){
-        return Optional.ofNullable(config.getString("jwt.issuer")).orElse("drwolf.it");
-    }
+	public Optional<String> getTokenFromRequest(Http.Request request) {
+		Optional<String> h = getTokenFromHeaders(request.getHeaders().asMap());
+		if (h.isPresent()) {
+			return h;
+		}
+		return getTokenFromQueryString(request.queryString());
+	}
 
-    protected Date getExpiration(){
-        return new Date(
-                new Date().getTime() +
-                        Optional.ofNullable(
-                                config.getDuration("jwt.expiration", TimeUnit.MILLISECONDS))
-                                .orElse(8*3600000l));
-    }
+	public <U> U getUser(Http.Request request, Class<U> userClass) {
+		DecodedJWT decoded = this.verify(getTokenFromRequest(request).orElseThrow(
+				() -> new HttpException("Token not found", HttpException.Status.UNAUTHORIZED)));
+		try {
+			JsonNode userAsJson = Json.parse(decoded.getClaim(getUserClaim()).asString());
+			return Json.fromJson(userAsJson, userClass);
+		} catch (Exception e) {
+			throw new HttpException("Invalid user data", HttpException.Status.UNAUTHORIZED);
+		}
+	}
 
-    public Optional<String> getTokenFromRequest(Http.Request request) {
-        Optional<String> h = getTokenFromHeaders(request.getHeaders().asMap());
-        if (h.isPresent()){
-            return h;
-        }
-        return getTokenFromQueryString(request.queryString());
-    }
+	protected String getUserClaim() {
+		return Optional.of(this.config.getString("jwt.userClaim")).orElse("user");
+	}
 
-    public <U> U getUser(Http.Request request, Class<U> userClass) {
-        DecodedJWT decoded = this.verify(getTokenFromRequest(request).orElseThrow(() -> new HttpException("Token not found",
-                HttpException.Status.UNAUTHORIZED)));
-        try {
-            JsonNode userAsJson = Json.parse(decoded.getClaim(getUserClaim()).asString());
-            return Json.fromJson(userAsJson, userClass);
-        } catch (Exception e) {
-            throw new HttpException("Invalid user data", HttpException.Status.UNAUTHORIZED);
-        }
-    }
+	private DecodedJWT verify(String token) {
+		try {
+			return this.delegate.verify(token);
+		} catch (Exception e) {
+			throw new HttpException("Invalid token", e, HttpException.Status.UNAUTHORIZED);
+		}
+	}
 
-    private Optional<String> getTokenFromHeaders(Map<String, List<String>> headers) {
-        List<String> headerAuth = headers.get("Authorization");
-        if ((headerAuth != null) && (headerAuth.size() == 1) && (headerAuth.get(0) != null)) {
-            String authorization = headerAuth.get(0);
-            return Optional.of(authorization.replace("Bearer ", ""));
-        }
-        return Optional.empty();
-    }
-
-    private Optional<String> getTokenFromQueryString(Map<String, String[]> queryString) {
-        if (queryString != null && queryString.containsKey("token")) {
-            String[] tokens = queryString.get("token");
-            if (tokens != null && tokens.length > 0) {
-                return Optional.of(tokens[0]);
-            }
-        }
-        return Optional.empty();
-    }
-
-    private DecodedJWT verify(String token) {
-        try {
-            return this.delegate.verify(token);
-        } catch (Exception e){
-            throw new HttpException("Invalid token", e, HttpException.Status.UNAUTHORIZED);
-        }
-    }
-
+	public DecodedJWT verifyJWKS(String accessToken) {
+		try {
+			DecodedJWT decodedJwt = JWT.decode(accessToken);
+			Jwk jwk = jwkProvider.get(decodedJwt.getKeyId());
+			Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+			Verification verifier = JWT.require(algorithm);
+			verifier.build().verify(decodedJwt);
+			return decodedJwt;
+		} catch (JwkException e) {
+			throw new HttpException("Error verifying JWKS Token", e);
+		}
+	}
 
 }
